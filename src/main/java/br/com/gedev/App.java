a44d949d38c5635d5673ae21dc566906e7054a0a
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-01-24 23:10:54 -0300

    iniciando aplicação

---
+++ src/main/java/br/com/gedev/App.java
@@ -0,0 +1,9 @@
+package br.com.gedev;
+
+import br.com.gedev.controllers.InteligenciaCentralController;
+
+public class App {
+    public static void main(String[] args) {
+        new InteligenciaCentralController().iniciar();
+    }
+}
--- src/main/java/br/com/gedev/controllers/InteligenciaCentralController.java
+++ src/main/java/br/com/gedev/controllers/InteligenciaCentralController.java
@@ -4,6 +4,7 @@ import br.com.gedev.enums.RacaRebelde;
 import br.com.gedev.exceptions.RebeldeReprovadoException;
 import br.com.gedev.models.Rebelde;
 import br.com.gedev.views.CadastroRebeldeView;
+import br.com.gedev.views.MenuView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +16,29 @@ public class InteligenciaCentralController {
         rebeldes = new ArrayList<Rebelde>();
     }
 
-    public void cadastrarRebelde() {
+
+    public void iniciar() {
+        while (true) {
+            menu();
+        }
+    }
+
+    private void menu() {
+        switch (MenuView.askOpcao()) {
+            case 1:
+                cadastrarRebelde();
+                break;
+
+            case 0:
+                System.exit(0);
+
+            default:
+                System.out.println("Opção não encontrada.");
+                break;
+        }
+    }
+
+    private void cadastrarRebelde() {
         String nome = CadastroRebeldeView.askNomeRebelde();
         int idade = CadastroRebeldeView.askIdadeRebelde();
         RacaRebelde raca = CadastroRebeldeView.askRacaRebelde();
