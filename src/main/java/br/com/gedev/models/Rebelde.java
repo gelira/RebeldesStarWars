a8c83533fb5a1e0370b885f51e853d1354185546
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-02-13 09:50:08 -0300

    corrigindo constraint de validação de nome

--- src/main/java/br/com/gedev/models/Rebelde.java
+++ src/main/java/br/com/gedev/models/Rebelde.java
@@ -1,9 +1,9 @@
 package br.com.gedev.models;
 
 import br.com.gedev.enums.RacaRebelde;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,8 +11,7 @@ import lombok.Data;
 @Builder
 public class Rebelde {
     @NotNull(message = "O nome do rebelde é obrigatório.")
-    @Min(value = 1, message = "O nome do rebelde não pode ser vazio.")
-    @Max(value = 10, message = "Resuma o nome desse rebelde.")
+    @Size(min = 1, max = 20, message = "O nome do rebelde deve ter entre 1 e 20 caracteres.")
     private String nome;
 
     @Min(value = 0, message = "Não existe idade negativa, meu patrão!")
