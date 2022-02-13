f52d5c796769c3a8f377566e92ae8e82d033f576
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-02-13 11:02:00 -0300

    comparação "maior que" entre rebeldes

--- src/main/java/br/com/gedev/models/Rebelde.java
+++ src/main/java/br/com/gedev/models/Rebelde.java
@@ -1,6 +1,8 @@
 package br.com.gedev.models;
 
 import br.com.gedev.enums.RacaRebelde;
+import br.com.gedev.enums.RebeldeAttribute;
+import br.com.gedev.exceptions.UnhandledRebeldeAttributeException;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -19,4 +21,32 @@ public class Rebelde {
 
     @NotNull(message = "A raça do rebelde é obrigatória.")
     private RacaRebelde raca;
+
+    public boolean greaterThan(Rebelde rebelde, RebeldeAttribute attribute) throws UnhandledRebeldeAttributeException {
+        if (RebeldeAttribute.NOME.equals(attribute)) {
+            return nomeGreaterThan(rebelde);
+        }
+
+        if (RebeldeAttribute.IDADE.equals(attribute)) {
+            return idadeGreaterThan(rebelde);
+        }
+
+        if (RebeldeAttribute.RACA.equals(attribute)) {
+            return racaGreaterThan(rebelde);
+        }
+
+        throw new UnhandledRebeldeAttributeException();
+    }
+
+    private boolean nomeGreaterThan(Rebelde rebelde) {
+        return nome.compareTo(rebelde.getNome()) > 0;
+    }
+
+    private boolean idadeGreaterThan(Rebelde rebelde) {
+        return idade > rebelde.getIdade();
+    }
+
+    private boolean racaGreaterThan(Rebelde rebelde) {
+        return raca.toString().compareTo(rebelde.getRaca().toString()) > 0;
+    }
 }
