3e84bf74251b33d3b9c01fd60594adff487f2545
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-02-13 13:03:38 -0300

    geração de relatório e ordenação dos rebeldes

--- src/main/java/br/com/gedev/controllers/InteligenciaCentralController.java
+++ src/main/java/br/com/gedev/controllers/InteligenciaCentralController.java
@@ -1,9 +1,12 @@
 package br.com.gedev.controllers;
 
+import br.com.gedev.enums.RebeldeAttribute;
 import br.com.gedev.exceptions.RebeldeReprovadoException;
+import br.com.gedev.exceptions.UnhandledRebeldeAttributeException;
 import br.com.gedev.models.Rebelde;
 import br.com.gedev.views.CadastroRebeldeView;
 import br.com.gedev.views.MenuView;
+import br.com.gedev.views.OrdenacaoView;
 import lombok.Cleanup;
 
 import java.io.FileNotFoundException;
@@ -11,6 +14,7 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class InteligenciaCentralController {
     private List<Rebelde> rebeldes;
@@ -57,6 +61,68 @@ public class InteligenciaCentralController {
         }
     }
 
+    private void relatorioRebeldes() {
+        RebeldeAttribute attribute = OrdenacaoView.askRebeldeAttribute();
+        boolean ordemInversa = OrdenacaoView.askOrdemInversa();
+
+        try {
+            Rebelde[] arrayRebeldesOrdenado = ordenarRebeldes(attribute);
+
+            if (ordemInversa) {
+                arrayRebeldesOrdenado = ordemInversa(arrayRebeldesOrdenado);
+            }
+
+            for (Rebelde rebelde : arrayRebeldesOrdenado) {
+                System.out.printf("%s%n%n", rebelde);
+            }
+
+            salvarRebeldes(arrayRebeldesOrdenado);
+        }
+        catch (UnhandledRebeldeAttributeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private Rebelde[] ordenarRebeldes(RebeldeAttribute attribute) throws UnhandledRebeldeAttributeException {
+        Object[] arrayAux = rebeldes.toArray();
+
+        Rebelde[] arrayRebeldes = new Rebelde[arrayAux.length];
+        int arrayMaxIndex = arrayRebeldes.length - 1;
+
+        for (int i = 0; i <= arrayMaxIndex; i ++) {
+            arrayRebeldes[i] = (Rebelde) arrayAux[i];
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < arrayMaxIndex; i ++) {
+            for (int j = 0; j < arrayMaxIndex - i; j ++) {
+                if (arrayRebeldes[j].greaterThan(arrayRebeldes[j + 1], attribute)) {
+                    Rebelde temp = arrayRebeldes[j];
+                    arrayRebeldes[j] = arrayRebeldes[j + 1];
+                    arrayRebeldes[j + 1] = temp;
+                }
+            }
+        }
+
+        return arrayRebeldes;
+    }
+
+    private Rebelde[] ordemInversa(Rebelde[] rebeldes) {
+        Stack<Rebelde> stackRebeldes = new Stack();
+
+        for (Rebelde rebelde : rebeldes) {
+            stackRebeldes.push(rebelde);
+        }
+
+        int c = 0;
+        while (!stackRebeldes.isEmpty()) {
+            rebeldes[c] = stackRebeldes.pop();
+            c ++;
+        }
+
+        return rebeldes;
+    }
+
     private void salvarRebeldes() {
         if (!relatorioGerado) {
             salvarRebeldes((Rebelde[]) rebeldes.toArray());
@@ -68,7 +134,7 @@ public class InteligenciaCentralController {
         try {
             writer = new PrintWriter("rebeldes.txt", "UTF-8");
             for (Rebelde rebelde : rebeldes) {
-                writer.println(rebelde);
+                writer.printf("%s%n%n", rebelde);
             }
             relatorioGerado = true;
         }
